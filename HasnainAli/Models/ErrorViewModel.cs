41cd27e0c3086b92cd07bdeb851988a7f3545551
HassnainAli <ahassnainsaleem964@gmail.com>
2024-03-29 20:31:31 -0700

    Add project files.

---
+++ HasnainAli/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HasnainAli.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
